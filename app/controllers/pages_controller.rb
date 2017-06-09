759aff829010dd2264357e1df3dda143b4d61597
stephane delatorre <s.dlt@hotmail.fr>
2017-06-09 21:55:07 +0200

    fixed raid sorting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @upcoming_raids = Raid.where('date > ?', Time.now)
-    @past_raids = Raid.where('date <= ?', Time.now)
+    @upcoming_raids = Raid.where('date > ?', Time.now.strftime('%Y-%m-%d %H:%M:%S'))
+    @past_raids = Raid.where('date <= ?', Time.now.strftime('%Y-%m-%d %H:%M:%S'))
   end
 end
