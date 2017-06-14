0a3d30ad300a69e0974fa67514cdc70700905543
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-06-14 18:19:26 +0200

    changed domain

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   user_name:              ENV['SENDGRID_USERNAME'],
   password:               ENV['SENDGRID_PASSWORD'],
-  domain:                 'http://localhost:3000',
+  domain:                 'https://tchinontwitch.herokuapp.com',
   address:                'smtp.sendgrid.net',
   port:                   587,
   authentication:         :plain,
