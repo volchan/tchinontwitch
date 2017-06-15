7a57fa5b606ba2f013a632cebe56db90037bea3d
stephane delatorre <s.dlt@hotmail.fr>
2017-06-15 18:17:10 +0200

    change to try devise email fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,15 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "https://tchinontwitch.herokuapp.com" }
+  ActionMailer::Base.smtp_settings = {
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :address              => "smtp.sendgrid.net",
+    :port                 => 587,
+    :enable_starttls_auto => true,
+    :authentication       => :plain,
+    :domain               => "yourdomain.com"
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -57,7 +67,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "tchinontwitch_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
