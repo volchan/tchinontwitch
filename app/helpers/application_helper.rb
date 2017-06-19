a9de3c26e79d827772753d89e2812120f027fe17
stephane delatorre <s.dlt@hotmail.fr>
2017-06-19 09:16:20 +0200

    changed toon pic display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def toon_picture(toon)
-    "background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('https://render-eu.worldofwarcraft.com/character/#{toon.thumbnail}');"
+    "background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('https://render-eu.worldofwarcraft.com/character/#{toon.thumbnail.gsub('avatar', 'inset')}');"
   end
 
   def toon_picture_accepted(toon)
