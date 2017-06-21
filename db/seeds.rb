d0a736e1b4d21a2ca9a92d41e6de905500da81fa
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-06-21 23:43:14 +0200

    changed seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,7 @@
 #
 # User.destroy_all
 # Realm.destroy_all
-Tag.destroy_all
-Raid.destroy_all
+# Raid.destroy_all
 #
 # print 'seeding admin...'
 #
@@ -16,46 +15,46 @@ Raid.destroy_all
 #
 # puts 'done seeding admin...'
 #
-# print 'seeding realms...'
-#
-# realm_call_url = "https://eu.api.battle.net/wow/realm/status?locale=en_GB&apikey=29vbsw7mjp4bjk2qqczb2ckxv5yhh5me"
-# realm_call = RestClient.get(realm_call_url)
-# parsed_realm_call = JSON.parse(realm_call)
-#
-# parsed_realm_call['realms'].each do |realm|
-#   Realm.create!(
-#     name: realm['name'],
-#     slug: realm['slug']
-#   )
-#   print "seeded #{realm['name']}..."
-# end
-#
-# puts 'done seeding realms....'
-#
-# print 'seeding dungeons...'
-#
-# dungeons_api_call = RestClient.get('https://eu.api.battle.net/wow/zone/?locale=en_GB&apikey=29vbsw7mjp4bjk2qqczb2ckxv5yhh5me')
-# dungeons_parsed_call = JSON.parse(dungeons_api_call)
-# dungeons_parsed_call['zones'].each do |zone|
-#   next unless zone['isRaid']
-#   Dungeon.create!(name: zone['name'], slug: zone['urlSlug'])
-#   print "seeded #{zone['name']}..."
-# end
-#
-# puts 'done seeding dungeons...'
+print 'seeding realms...'
 
-print 'seeding raids...'
+realm_call_url = "https://eu.api.battle.net/wow/realm/status?locale=en_GB&apikey=29vbsw7mjp4bjk2qqczb2ckxv5yhh5me"
+realm_call = RestClient.get(realm_call_url)
+parsed_realm_call = JSON.parse(realm_call)
 
-dungeons = Dungeon.all
-30.times do |i|
-  new_raid_infos = {}
-  new_raid_infos[:dungeon] = dungeons.sample
-  new_raid_infos[:date] = Time.now + rand(-10..5).day
-  new_raid_infos[:difficulty] = rand(0..2)
-  new_raid_infos[:faction] = rand(0..1)
-  new_raid_infos[:leader] = Toon.first
-  Raid.create!(new_raid_infos)
-  print "seeded raid #{i + 1}..."
+parsed_realm_call['realms'].each do |realm|
+  Realm.create!(
+    name: realm['name'],
+    slug: realm['slug']
+  )
+  print "seeded #{realm['name']}..."
 end
 
-puts 'done seeding raids...'
+puts 'done seeding realms....'
+#
+print 'seeding dungeons...'
+
+dungeons_api_call = RestClient.get('https://eu.api.battle.net/wow/zone/?locale=en_GB&apikey=29vbsw7mjp4bjk2qqczb2ckxv5yhh5me')
+dungeons_parsed_call = JSON.parse(dungeons_api_call)
+dungeons_parsed_call['zones'].each do |zone|
+  next unless zone['isRaid']
+  Dungeon.create!(name: zone['name'], slug: zone['urlSlug'])
+  print "seeded #{zone['name']}..."
+end
+
+puts 'done seeding dungeons...'
+
+# print 'seeding raids...'
+#
+# dungeons = Dungeon.all
+# 30.times do |i|
+#   new_raid_infos = {}
+#   new_raid_infos[:dungeon] = dungeons.sample
+#   new_raid_infos[:date] = Time.now + rand(-10..5).day
+#   new_raid_infos[:difficulty] = rand(0..2)
+#   new_raid_infos[:faction] = rand(0..1)
+#   new_raid_infos[:leader] = Toon.first
+#   Raid.create!(new_raid_infos)
+#   print "seeded raid #{i + 1}..."
+# end
+#
+# puts 'done seeding raids...'
