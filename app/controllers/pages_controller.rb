cbe76fe90a81430dc0a71fc83be808bfad854f5f
stephane delatorre <s.dlt@hotmail.fr>
2017-06-22 12:54:29 +0200

    refacto pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @upcoming_raids = Raid.where('date > ?', Time.now.in_time_zone("Paris")).order(date: :asc)
-    @past_raids = Raid.where('date <= ?', Time.now.in_time_zone("Paris")).order(date: :desc)
+    @upcoming_raids = Raid.where('date > ?', Time.now.in_time_zone('Paris')).order(date: :asc)
+    @past_raids = Raid.where('date <= ?', Time.now.in_time_zone('Paris')).order(date: :desc)
   end
 end
